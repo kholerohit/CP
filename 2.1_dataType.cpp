f70b0ee2a44ae5bc79483e62d8f0af111504582a
Khole Rohit <kholerocky9656@yahoo.com>
2022-01-14 12:18:14 +0530

    -- Adding 4 days preparation videos

---
+++ 2.1_dataType.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x = 122;
+    cout<< "Size of Int: "<<sizeof(x)<<" Byte "<<endl;
+
+    char ch = 'a';
+    cout<<"Size of Char :"<< sizeof(ch)<<" Byte"<<endl;
+
+    float f = 3.14f;
+    cout<<"Size of Float :"<<sizeof(f)<<" Byte"<<endl;
+
+    double d = 1.3333454234;
+    cout<<"Size of Double :"<<sizeof(d)<<" Byte"<<endl;
+
+    return 0;
+}
